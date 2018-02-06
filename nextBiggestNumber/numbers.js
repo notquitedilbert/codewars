33373da9a77bd53c04f2b8576522efc2a8152fe3
Andrew Shepherd <andrew.shepherd@dwp.gsi.gov.uk>
2018-02-06 14:12:32 +0000

    wip change the fors to whiles

--- nextBiggestNumber/numbers.js
+++ nextBiggestNumber/numbers.js
@@ -6,34 +6,63 @@ function nextBigger(n) {
   var nn = n.toString().split('')
   // walk from right to left looking for numbers to swap
 
-  for (var i = nn.length - 2; i >= 0; i--) {
-    // find the spot to swap the numbers
+  var i = nn.length - 2;
+  var swap = null;
+  do {
     if (nn[i] < nn[i + 1]) {
-      // nn[i] is the number to swap out
-      var swap = nn[i]
-      // nn[i] = nn[i + 1]
-      // nn[i + 1] = swap
-
-      var lookin = nn.slice(i,nn.length).sort()
-      for(var j=0;j<lookin.length-1;j++){
-        if(swap < lookin[j]){
-            nn[i] = lookin[j]
-            nn[i+j] = swap
+      var swap = nn[i];
+      var match = null;
+      var j = 0;
+      var lookin = nn.slice(i, nn.length).sort()
+      do {
+        if (swap < lookin[j]) {
+          nn[i] = lookin[j]
+          lookin[j] = swap
+          match = true
         }
-      }
-
+        j++
+      } while (match == null);
 
       // sort the end numbers into ascending order
-      var endOf = nn.slice(i + 1, nn.length).sort().join('')
+      var endOf = lookin.sort().join('')
       // get the start of the numbers
       var startOf = nn.slice(0, i + 1).join('')
       // join them together and return the new number
-
-      console.log(n,nn,lookin,startOf,endOf)
       return parseInt(startOf + endOf)
     }
-  }
-  return -1
+
+    i--
+  } while (i >= 0 || swap == null)
+
+  // for (var i = nn.length - 2; i >= 0; i--) {
+  //   // find the spot to swap the numbers
+  //   if (nn[i] < nn[i + 1]) {
+  //     // nn[i] is the number to swap out
+  //     var swap = nn[i]
+  //     // nn[i] = nn[i + 1]
+  //     // nn[i + 1] = swap
+
+  //     var lookin = nn.slice(i, nn.length).sort()
+  //     for (var j = 0; j < lookin.length - 1; j++) {
+  //       if (swap < lookin[j]) {
+  //         nn[i] = lookin[j]
+  //         lookin[j] = swap
+  //       }
+  //     }
+
+
+  //     // sort the end numbers into ascending order
+  //     var endOf = lookin.sort().join('')
+  //     // get the start of the numbers
+  //     var startOf = nn.slice(0, i + 1).join('')
+  //     // join them together and return the new number
+
+  //     console.log(n, nn, lookin, startOf, endOf)
+  //     return parseInt(startOf + endOf)
+  //   }
+  // }
+  // return -1
 }// end of function
 
+console.log(nextBigger(90986))
 module.exports.nextBigger = nextBigger;
